761c643f0c05cc3ee0e5144118661b4f9fce8716
nassimmninou <mninou.nas@gmail.com>
2023-05-26 20:08:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import Header from "./Header";
+import Footer from "./Footer";
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-function App() {
+
+export default function App() {
+  const [restaurants, setRestaurants] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
+  const [selectedZone, setSelectedZone] = useState("");
+  const [selectedRestaurant, setSelectedRestaurant] = useState("");
+  const containerStyle = {
+    width: '400px',
+    height: '400px'
+  };
+  
+  const center = {
+    lat: 0.0,
+    lng: 0.0
+  };
+  
+  const zoom = 80;
+  
+  const [selectedRestaurantLocation, setSelectedRestaurantLocation] = useState({
+    lat: 0.0,
+    lng: 0.0
+  });
+  
+  
+
+  useEffect(() => {
+    Axios.get("https://data-hazel-six.vercel.app/restaurants").then((res) => {
+      setRestaurants(res.data);
+    });
+  }, []);
+
+  const handleCitySelect = (event) => {
+    setSelectedCity(event.target.value);
+    setSelectedZone("");
+    setSelectedRestaurant("");
+  };
+
+  const handleZoneSelect = (event) => {
+    setSelectedZone(parseInt(event.target.value));
+    setSelectedRestaurant("");
+  };
+
+
+  const handleRestaurantSelect = (event) => {
+    setSelectedRestaurant(event.target.value);
+    var title = event.target.value;
+    restaurants.forEach((restaurant) => {
+      if (restaurant.title === title) {
+        setSelectedRestaurantLocation({
+          lat: restaurant.location.lat,
+          lng: restaurant.location.lng
+        });
+      }
+    });
+  };
+  
+
+  useEffect(() => {
+    if (selectedCity && selectedZone) {
+      const startIndex = (selectedZone - 1) * 20;
+      const endIndex = selectedZone * 20;
+      Axios.get(`https://data-hazel-six.vercel.app/restaurants/${selectedCity}`).then(
+      (res) => {
+        setRestaurants(res.data.slice(startIndex, endIndex));
+      }
+    );
+  }
+}, [selectedCity, selectedZone]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      
+      <select value={selectedCity} onChange={handleCitySelect}>
+        <option value="">Select a city</option>
+        <option value="Casablanca">Casablanca</option>
+        <option value="Tanger">Tanger</option>
+        <option value="El Jadida">El jadida</option>
+        {/* add more cities as needed */}
+      </select>
+      <select value={selectedZone} onChange={handleZoneSelect}>
+        <option value="">Select a zone</option>
+        <option value="1">Zone 1</option>
+        <option value="2">Zone 2</option>
+        <option value="3">Zone 3</option>
+        <option value="4">Zone 4</option>
+      </select>
+      <select value={selectedRestaurant} onChange={handleRestaurantSelect}>
+        <option value="">Select a restaurant</option>
+        {restaurants.map((restaurant) => (
+          <option value={restaurant.id} key={restaurant.id}>
+            {restaurant.title}
+          </option>
+        ))}
+      </select>
+      <LoadScript
+      googleMapsApiKey="AIzaSyD0yMlmaWjIpewkPhPyFnOJHySVYeqvW-4"
+    >
+      <GoogleMap
+  mapContainerStyle={containerStyle}
+  center={selectedRestaurantLocation}
+  zoom={zoom}
+>
+  <Marker position={selectedRestaurantLocation} />
+  {restaurants.map((restaurant) => (
+    <Marker
+      key={restaurant.id}
+      position={{ lat: restaurant.lat, lng: restaurant.lng }}
+    />
+  ))}
+</GoogleMap>
+
+
+    </LoadScript>
+      <Footer />
+    </>
   );
 }
-
-export default App;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ backgroundColor: "#282c34", height: "50px", color: "white", display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <h1>SEARCH APP FOR RESTAURANTS</h1>
+    </header>
+  );
+};
+
+export default Header;
